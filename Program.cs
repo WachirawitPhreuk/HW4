d17a5d2e0b9154d8c5556b01413a18c9c2855e0e
WachirawitPhreuk <wachirawit.pruk@mail.kmutt.ac.th>
2022-12-12 21:35:19 +0700

    HW4
    ครั้งที่ 4 (ผมไม่รู้เรื่อง tree)

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Tree<string> t = new Tree<string>();
+        string name;
+
+        while(true) {
+        name = Console.ReadLine();
+        int days = int.Parse(Console.ReadLine());
+        
+        }
+    }
+}
---
+++ Tree.cs
@@ -0,0 +1,77 @@
+class Tree<T>
+{
+    private TreeNode<T> root = null;
+    private int length = 0;
+
+    public void AddSibling(int index, T value)
+    {
+        TreeNode<T> node = new TreeNode<T>(value);
+        TreeNode<T> ptr = this.GetTreeNode(index);
+        node.SetNext(ptr.Next());
+        ptr.SetNext(node);
+        this.length++;
+    }
+
+    public void AddChild(int index, T value)
+    {
+        TreeNode<T> node = new TreeNode<T>(value);
+        if(index == -1)
+        {
+            node.SetChild(this.root);
+            this.root = node;
+        }
+        else
+        {
+            TreeNode<T> ptr = this.GetTreeNode(index);
+            node.SetChild(ptr.Child());
+            ptr.SetChild(node);
+        }
+        this.length++;
+    }
+
+    public int GetLength()
+    {
+        return this.length;
+    }
+
+    public T Get(int index)
+    {
+        TreeNode<T> ptr = this.GetTreeNode(index);
+        return ptr.GetValue();
+    }
+
+    private TreeNode<T> GetTreeNode(int index)
+    {
+        int traverseStep = index;
+        return this.Traverse(this.root, ref traverseStep);
+    }
+
+    private TreeNode<T> Traverse(TreeNode<T> currentTreeNode, ref int traverseStep)
+    {
+        TreeNode<T> ptr = currentTreeNode;
+
+        if(traverseStep > 0 && currentTreeNode.Child() != null)
+        {
+            traverseStep--;
+            ptr = this.Traverse(currentTreeNode.Child(), ref traverseStep);
+        }
+
+        if(traverseStep > 0 && currentTreeNode.Next() != null)
+        {
+            traverseStep--;
+            ptr = this.Traverse(currentTreeNode.Next(), ref traverseStep);
+        }
+
+        return ptr;
+
+        /* เนื่องจากผมเขียนทฤษฎีได้ แต่ผมทำปฏิบัติไม่ได้ (ผมไม่เข้าใจเรื่องนี้จริงๆ orz
+        )
+        ต่อจาก Program
+        หลังจากที่ใส่ค่า name และ days มาแล้ว ค่าทั้งสองจะถูกเก็บในตัวแปรใน TreeNode
+        ทำการเก็บค่่าด้วยการ ref
+        
+        ใช้ if เมื่อมีคำใดซ้ำกับ name ในตัวเก็บ โปรแกรมจะหยุดทำงาน
+        
+         */
+    }
+}
